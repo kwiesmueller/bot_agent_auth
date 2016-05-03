11bf8997fb99d0836feaf03a432320d5b7b711cd
Benjamin Borbe <bborbe@rocketnews.de>
2016-05-03 09:25:43 +0200

    fmt

--- bin/bot_agent_auth/bot_agent_auth.go
+++ bin/bot_agent_auth/bot_agent_auth.go
@@ -5,34 +5,34 @@ import (
 	"os"
 
 	"github.com/bborbe/bot_agent/request_consumer"
+	"github.com/bborbe/bot_agent_auth/auth"
 	"github.com/bborbe/bot_agent_auth/creator"
 	"github.com/bborbe/bot_agent_auth/lister"
 	flag "github.com/bborbe/flagenv"
 	http_client_builder "github.com/bborbe/http/client_builder"
 	http_requestbuilder "github.com/bborbe/http/requestbuilder"
 	"github.com/bborbe/log"
-	"github.com/bborbe/bot_agent_auth/auth"
 )
 
 const (
-	PARAMETER_LOGLEVEL = "loglevel"
-	PARAMETER_NSQ_LOOKUPD = "nsq-lookupd-address"
-	PARAMETER_NSQD = "nsqd-address"
-	DEFAULT_BOT_NAME = "auth"
-	PARAMETER_BOT_NAME = "bot-name"
-	PARAMETER_AUTH_ADDRESS = "auth-address"
-	PARAMETER_AUTH_APPLICATION_NAME = "auth-application-name"
+	PARAMETER_LOGLEVEL                  = "loglevel"
+	PARAMETER_NSQ_LOOKUPD               = "nsq-lookupd-address"
+	PARAMETER_NSQD                      = "nsqd-address"
+	DEFAULT_BOT_NAME                    = "auth"
+	PARAMETER_BOT_NAME                  = "bot-name"
+	PARAMETER_AUTH_ADDRESS              = "auth-address"
+	PARAMETER_AUTH_APPLICATION_NAME     = "auth-application-name"
 	PARAMETER_AUTH_APPLICATION_PASSWORD = "auth-application-password"
 )
 
 var (
-	logger = log.DefaultLogger
-	logLevelPtr = flag.String(PARAMETER_LOGLEVEL, log.INFO_STRING, log.FLAG_USAGE)
-	nsqLookupdAddressPtr = flag.String(PARAMETER_NSQ_LOOKUPD, "", "nsq lookupd address")
-	nsqdAddressPtr = flag.String(PARAMETER_NSQD, "", "nsqd address")
-	botNamePtr = flag.String(PARAMETER_BOT_NAME, DEFAULT_BOT_NAME, "bot name")
-	authAddressPtr = flag.String(PARAMETER_AUTH_ADDRESS, "", "auth address")
-	authApplicationNamePtr = flag.String(PARAMETER_AUTH_APPLICATION_NAME, "", "auth application name")
+	logger                     = log.DefaultLogger
+	logLevelPtr                = flag.String(PARAMETER_LOGLEVEL, log.INFO_STRING, log.FLAG_USAGE)
+	nsqLookupdAddressPtr       = flag.String(PARAMETER_NSQ_LOOKUPD, "", "nsq lookupd address")
+	nsqdAddressPtr             = flag.String(PARAMETER_NSQD, "", "nsqd address")
+	botNamePtr                 = flag.String(PARAMETER_BOT_NAME, DEFAULT_BOT_NAME, "bot name")
+	authAddressPtr             = flag.String(PARAMETER_AUTH_ADDRESS, "", "auth address")
+	authApplicationNamePtr     = flag.String(PARAMETER_AUTH_APPLICATION_NAME, "", "auth application name")
 	authApplicationPasswordPtr = flag.String(PARAMETER_AUTH_APPLICATION_PASSWORD, "", "auth application password")
 )
 
@@ -51,14 +51,14 @@ func main() {
 }
 
 func do(nsqdAddress string, nsqLookupdAddress string, botname string, authAddress string, authApplicationName string, authApplicationPassword string) error {
-	requestConsumer, err := createRequestConsumer(nsqdAddress, nsqLookupdAddress, botname, authAddress , authApplicationName , authApplicationPassword )
+	requestConsumer, err := createRequestConsumer(nsqdAddress, nsqLookupdAddress, botname, authAddress, authApplicationName, authApplicationPassword)
 	if err != nil {
 		return err
 	}
 	return requestConsumer.Run()
 }
 
-func createRequestConsumer(nsqdAddress string, nsqLookupdAddress string, botname string,authAddress string, authApplicationName string, authApplicationPassword string) (request_consumer.RequestConsumer, error) {
+func createRequestConsumer(nsqdAddress string, nsqLookupdAddress string, botname string, authAddress string, authApplicationName string, authApplicationPassword string) (request_consumer.RequestConsumer, error) {
 	if len(nsqLookupdAddress) == 0 {
 		return nil, fmt.Errorf("parameter %s missing", PARAMETER_NSQ_LOOKUPD)
 	}
--- bin/bot_agent_auth/bot_agent_auth_test.go
+++ bin/bot_agent_auth/bot_agent_auth_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestCreateRequestConsumer(t *testing.T) {
-	createRequestConsumer, err := createRequestConsumer("nsqd", "nsqlookupd", "testbot", "auth-api","auth-app-name","auth-app-pw")
+	createRequestConsumer, err := createRequestConsumer("nsqd", "nsqlookupd", "testbot", "auth-api", "auth-app-name", "auth-app-pw")
 	if err := AssertThat(err, NilValue()); err != nil {
 		t.Fatal(err)
 	}
